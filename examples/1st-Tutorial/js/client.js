d32150a40574aa9cdb1294ebb474a47257548d43
Bernardo Marques Graça <bernardo.marquesg@gmail.com>
2017-06-19 19:09:06 +0100

    1st Tutorial Example Complete

---
+++ examples/1st-Tutorial/js/client.js
@@ -0,0 +1,50 @@
+
+const hypertyURI = (hyperty_domain, hyperty) => `hyperty-catalogue://catalogue.${hyperty_domain}/.well-known/hyperty/${hyperty}`;
+let codeGeneratorReporter
+
+$(document).ready(function () {
+
+  rethink.default.install({
+    domain: 'hysmart.rethink.ptinovacao.pt',
+    development: true,
+    runtimeURL: 'hyperty-catalogue://catalogue.hysmart.rethink.ptinovacao.pt/.well-known/runtime/Runtime'
+  }).then((runtime) => {
+    loadHyperty(runtime)
+  });
+})
+
+
+function loadHyperty(runtime) {
+  runtime.requireHyperty(hypertyURI('hysmart.rethink.ptinovacao.pt', 'CodeGeneratorReporter'))
+  .then((CodeGeneratorReporter) => {
+    codeGeneratorReporter = CodeGeneratorReporter.instance
+    $('#create').show()
+    $('#buttonCreate').on('click', create)
+  });
+}
+
+function create() {
+  let emails = []
+  emails.push($('#createInput').val())
+
+  codeGeneratorReporter.create(emails).then((result) => {
+    $('#createInput').val('')
+    $('#create').hide()
+    $('#code').show()
+    $('#buttonGenerate').on('click', generateCodeService)
+  });
+}
+
+function generateCodeService() {
+  $('#alert').hide()
+  $('#alert').text('')
+  $('#alert').children().remove()
+  let teamName = $('#inlineFormInput').val()
+  setTimeout(() => {
+    codeGeneratorReporter.generateCode(teamName).then((code) => {
+      $('#alert').show()
+      $('#inlineFormInput').val('')
+      $('#alert').append('<strong>Well done!</strong> Code: ' + code)
+    });
+  }, 1000);
+}
